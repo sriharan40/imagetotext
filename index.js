870c8148fbd4c934d91518164a6afef4b7583145
sriharan40 <sriharan40@gmail.com>
2017-02-22 00:47:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/upload', upload.single('image'), function(req, res, next) {
 // Choices are: faces, landmarks, labels, logos, properties, safeSearch, texts
 var types = ['text'];
 
-console.log("Req: "+req.tostring());
+console.log("Req: "+req.toString());
   
 console.log("Path: "+req.file.path);
   
