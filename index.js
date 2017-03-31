d5eb0961ed2aef44b78dd6e56017d3c20e2d6179
sriharan40 <sriharan40@gmail.com>
2017-03-31 23:59:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,15 +50,18 @@ vision.detect(image_url, types, function(err, detections, apiResponse) {
 
       //var jsonOutput = JSON.parse(apiResponse);
       var texts = JSON.stringify(apiResponse.responses[0].textAnnotations[0].description);
+      var textsHtmlwithoutQuotes = texts.replace(/"/g, '');
+      var textWithNextline = textsHtmlwithoutQuotes.replace(/\\n/g, '</br>');
+      
+	  console.log("Check texts ::>>" + textWithNextline);
+	  
       var text_output = texts.replace(/\\n/g, '\n\n');	  
       text_output = '\n\n'+text_output;   
 	  var arr = text_output.split("MIDDLE NAME");
       console.log("Splitted text:" + arr[1]);	  
 	  var arr1 = arr[1].split("ADDRESS");
       console.log("Splitted text1:" + arr1[0]);
-      var textsHtmlwithoutQuotes = texts.replace(/"/g, '');
-      var textWithNextline = textsHtmlwithoutQuotes.replace(/\\n/g, '</br>');
-      console.log("Check texts ::>>" + textWithNextline);
+
       // Write out the JSON output of the Vision API
        //res.write(JSON.stringify(jsonObj.textAnnotations, null, 4));
       
